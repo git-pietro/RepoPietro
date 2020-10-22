decb97522a8382f4480bcaf860566955c2a8a100
pietr <pietr@Lifetime>
2020-10-22 16:07:50 +0200

    End

--- Project/src/Project01/BlueBook.java
+++ Project/src/Project01/BlueBook.java
@@ -1,10 +1,25 @@
 package Project01;
-import static pr.MakeItSimple.println;
+import static pr.MakeItSimple.*;
 public class BlueBook {
 
 	public static void main(String[] args) {
-		System.out.println("BlueBook");
-		println("Test me");
+		
+		print("Nenner: ");
+		
+		int nenner = readInt();
+		
+		print("Zähler: ");
+		
+		int zaeler = readInt();		
+		
+		println("Ergebnis: " + Math.pow(nenner, zaeler) );
+		
+		if (nenner ==0)
+			println("Diese Aktion ist nicht möglich, bitte nicht die Null benutzen.");
+		else println("Good:)");
 	}
 
-}
+	
+	}
+
+
