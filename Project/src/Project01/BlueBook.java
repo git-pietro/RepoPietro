079e42cc649c64996bbc7af544e5f5d9e7894437
pietr <pietr@Lifetime>
2020-10-22 15:03:15 +0200

    Test2

--- Project/src/Project01/BlueBook.java
+++ Project/src/Project01/BlueBook.java
@@ -1,10 +1,10 @@
 package Project01;
-
+import static pr.MakeItSimple.println;
 public class BlueBook {
 
 	public static void main(String[] args) {
 		System.out.println("BlueBook");
-
+		println("Test me");
 	}
 
 }
